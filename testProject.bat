7b521bb72d357d29abf3ab6fbad42a488b98e511
Wei-Chi Chen <weichic.tw@gmail.com>
2024-02-24 23:30:35 -0800

    Fix batch file variable naming bug

--- testProject.bat
+++ testProject.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set /p PROJ_IDX=Please enter the project index you are working on: 
-set PROJ_HOME = miniproject_%PROJ_IDX%
+set /p PROJ_IDX="Please enter the project index you are working on: "
+set PROJ_HOME=miniproject_%PROJ_IDX%
 
 mvn clean -f %PROJ_HOME%\pom.xml && ^
 mvn validate -f %PROJ_HOME%\pom.xml && ^
